79d1b165ce99a9fd3ac63548ab55ac31aa823fc3
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-16 22:49:29 +0530

    Used Integer object and compare to method to test the maximum number

---
+++ MaximumNoProgramUsingGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumNoProgramUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 Integers where given and will be compared with each other to show the Maximum Number.");
+            Console.WriteLine("First Number = 18, Second Number = 15, Third Number = 12");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            CompareNo.compareNo();
+        }
+    }
+}
