449ba23083244c9f2d9d670302f9852b54f61b40
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-17 16:04:20 +0530

    Extend the max method to take more then three Parameters

---
+++ MaximumNoProgramUsingGenerics/MaxCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumNoProgramUsingGenerics
+{
+    public class MaxCount<T> where T : IComparable<T>
+    {
+		List<T> maxValue = new List<T>();
+		public MaxCount(params T[] values)
+		{
+			this.ValuesBuilder(values);
+		}
+		public void ValuesBuilder(T[] maxValue)
+		{
+			foreach (T item in maxValue)
+			{
+				this.maxValue.Add(item);
+			}
+		}
+		public void Values()
+		{
+			this.maxValue.Sort();
+			Console.WriteLine(this.maxValue[this.maxValue.Count - 1]);
+		}
+	}
+}
